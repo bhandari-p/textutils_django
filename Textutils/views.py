bb1a3e7a62fdde9907a344cf0ed69ebb6fb92898
p_bhandari <147399832+bhandari-p@users.noreply.github.com>
2024-03-25 18:26:18 +0545

    added ss and bug fixed

---
+++ Textutils/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+def index(request):
+    # return HttpResponse("Home")
+    return render (request,'index.html')
+
+def analyze(request):
+    # get the text
+    djtext=request.GET.get('text','default')
+    removepunc=request.GET.get('removepunc','off')
+    fullcaps=request.GET.get('fullcaps','off')
+    newlineremover=request.GET.get('newlineremover','off')
+
+
+# check box value
+    if removepunc=="on":
+        punctuations='''/?:;"',(){[}]_*^%$#@! '''
+        analyzed=" "
+        for char in djtext:
+          if char not in punctuations:
+              analyzed=analyzed+char
+        params={'purpose':'removed punctuations','analyzed_text':analyzed}
+        djtext=analyzed
+         # analyze the text
+        # return render(request, 'analyze.html',params)
+    if fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params={'purpose':'Change to uppercase','analyzed_text':analyzed}    
+        djtext=analyzed
+        # return render(request,'analyze.html',params )
+    if newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char != " " :
+             analyzed=analyzed+char
+        params={'purpose':'New line Removed','analyzed_text':analyzed}    
+        djtext=analyzed
+        # return render(request,'analyze.html',params )
+
+    else:
+        return HttpResponse("Error")
+    return render(request,'analyze.html',params )
+
+
+def about_us(request):
+       return render(request,'about.html')
